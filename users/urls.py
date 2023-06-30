b31d9347dce3641374a5b7d8bda1df7b7b2cf584
greduan <me@greduan.com>
2023-06-30 21:29:45 +0200

    feat(users): add page to edit vehicle user data

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', login_required(views.index_get), name='index'),
     path('<str:user>/<str:vehicle_id>', login_required(views.user_vehicle_get), name='user_vehicle'),
+    path('<str:user>/<str:vehicle_id>/user_data', login_required(views.user_vehicle_user_data), name='user_vehicle_user_data'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 import json
 
 from django.shortcuts import render
-
-from summarytemplates.views import make_api_get_request
+from django.contrib import messages
+from summarytemplates.views import make_api_get_request, make_api_put_request
+from users.forms import VehicleUserDataForm
 
 
 def index_get(request):
@@ -20,13 +21,52 @@ def user_vehicle_get(request, user, vehicle_id):
     annual_fuel_cost = avg(null_conditional(data, 'vinAuditData.ownershipCost.fuelCost', 0))
     annual_maintenance_cost = avg(null_conditional(data, 'vinAuditData.ownershipCost.maintenanceCost', 0))
     vehicle = json.dumps(data, indent=4, sort_keys=True)
+    vehicle_user_data = json.dumps(data['userData'], indent=4, sort_keys=True)
     return render(request, 'users/user_vehicle.html', {
+        'user': user,
+        'vehicle_id': vehicle_id,
         'vehicle': vehicle,
+        'vehicle_user_data': vehicle_user_data,
         'average_market_value': average_market_value,
         'annual_insurance_cost': annual_insurance_cost,
         'annual_fuel_cost': annual_fuel_cost,
         'annual_maintenance_cost': annual_maintenance_cost,
     })
+
+
+def user_vehicle_user_data(request, user, vehicle_id):
+    vehicle = make_api_get_request(f'/vehicles/{vehicle_id}?username={user}')
+
+    if request.method == 'POST':
+        form = VehicleUserDataForm(request.POST)
+        if form.is_valid():
+            vehicle['userData'] = {
+                'insuranceRate': form['insurance_rate'].data,
+                'insuranceProvider': form['insurance_provider'].data,
+                'insuranceRenewalDate': form['insurance_renewal_date'].data,
+                'financingInterestRate': form['financing_interest_rate'].data,
+                'financingTermEnd': form['financing_term_end'].data,
+                'previousMaintenanceData': form['previous_maintenance_data'].data,
+            }
+            del vehicle['vinAuditData']
+            make_api_put_request(f'/vehicles/{vehicle_id}?username={user}', vehicle)
+            messages.success(request, 'Vehicle updated successfully')
+        else:
+            messages.error(request, 'Invalid data')
+    else:
+        user_data = vehicle['userData']
+        initial_data = {
+            'insurance_rate': user_data['insuranceRate'],
+            'insurance_provider': user_data['insuranceProvider'],
+            'insurance_renewal_date': user_data['insuranceRenewalDate'],
+            'financing_interest_rate': user_data['financingInterestRate'],
+            'financing_term_end': user_data['financingTermEnd'],
+            'previous_maintenance_data': user_data['previousMaintenanceData'],
+        }
+        form = VehicleUserDataForm(initial=initial_data)
+
+    vehicle = make_api_get_request(f'/vehicles/{vehicle_id}?username={user}')
+    return render(request, 'users/user_vehicle_user_data.html', {'user': user, 'vehicle': vehicle, 'form': form})
 
 
 def null_conditional(obj, attr_string, default=None):
